7f34b50517dd7a1fe8536f30b7cc8f8388819cec
junior Farias <junior@beetech.global>
2018-04-04 00:42:54 -0300

    feat(navigation): add navigation in app component

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import DeckList from './src/components/DeckList'
+import NewDeck from './src/components/NewDeck'
+import { white, gray } from './src/utils/colors'
 
-export default class App extends React.Component {
+const CustomStatusBar =  ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks'
+    },
+  },
+  newDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Decks'
+    }
+  },
+},
+  {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    style: {
+      height: 56,
+      backgroundColor: gray,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  }
+})
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View  style={{ flex: 1 }}>
+        <CustomStatusBar backgroundColor={gray} barStyle="light-content" />
+        <MainNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
